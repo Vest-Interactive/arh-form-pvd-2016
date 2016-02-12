c30bbf1da850daa4280438405a7b29d64fb99af5
Andrew Starosciak <andrewstarosciak@gmail.com>
2016-02-12 16:26:17 -0500

    analytics

--- html/ajax/signup.ajax.php
+++ html/ajax/signup.ajax.php
@@ -26,15 +26,15 @@ if(isset($_POST) && isset($_POST['register'])) {
 						  '</table>'.
 						  '<table cellpadding="0" cellspacing="0" border="0" align="center" width="600">'.
 						  '<tbody><tr>'.
-						  '<td width="175"> First Name </td>'.
+						  '<td width="175"> Name </td>'.
 						  '<td width="525">: '.$register['name'].'</td>'.
 						  '</tr>'.
 						  '<tr>'.
-						  '<td width="175"> Last Name </td>'.
+						  '<td width="175"> Phone  </td>'.
 						  '<td width="525">: '.$register['phone'].'</td>'.
 						  '</tr>'.
 						  '<tr>'.
-						  '<td width="175"> Company </td>'.
+						  '<td width="175"> Email </td>'.
 						  '<td width="525">: '.$register['email'].'</td>'.
 						  '</tr>'. 
 						  '<tr>'.
@@ -45,7 +45,7 @@ if(isset($_POST) && isset($_POST['register'])) {
 			$headers = "MIME-Version: 1.0" . "\r\n";
 			$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 			$headers .= 'From: no-reply@doyourlegshurt.com' . "\r\n";
-			$result = mail('andrews@vestadvertising.com', '[doyourlegshurt.com] ARH &amp; UK - Contact Request', $newMessage, $headers);			
+			$result = mail('chi223@uky.edu, sdamron@arh.org, dfertig@vestadvertising.com, andrews@vestadvertising.com', '[doyourlegshurt.com] Contact Request', $newMessage, $headers);			
 
 			if($result) {
 				echo json_encode(array( "success" => 1, "code" => 1));
